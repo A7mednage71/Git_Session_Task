16edc87ebce504181d18fe18a4778b4ad12fae7c
Ahmed Nageh <ahmdnajkh71@gmail.com>
2025-12-30 12:05:04 +0200

    Fix typo in output message and format code

--- hello_world.java
+++ hello_world.java
@@ -1,10 +1,9 @@
 
+class Main {
 
-class Main{
-    
-	   public static void main(String[] args) {
-        
-		System.out.println("Hello Wordl");
-	   }
-    
+	public static void main(String[] args) {
+
+		System.out.println("Hello Hallo World");
+	}
+
 }
